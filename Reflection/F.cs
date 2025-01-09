bb4e156c7c71a5bc1a5ddecfce856221a00e0e48
neverdietester <zdorov92@mail.ru>
2025-01-09 15:26:50 +0300

    Serialization

---
+++ Reflection/F.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    class F
+    {
+        public int i1, i2, i3, i4, i5;
+
+        public static F Get()
+        {
+            return new F() { i1 = 1, i2 = 2, i3 = 3, i4 = 4, i5 = 5 };
+        }
+
+        public string Serialize()
+        {
+            return $"{i1},{i2},{i3},{i4},{i5}";
+        }
+
+        public static F Deserialize(string str)
+        {
+            var parts = str.Split(',');
+            return new F
+            {
+                i1 = int.Parse(parts[0]),
+                i2 = int.Parse(parts[1]),
+                i3 = int.Parse(parts[2]),
+                i4 = int.Parse(parts[3]),
+                i5 = int.Parse(parts[4])
+            };
+        }
+    }
+}
---
+++ Reflection/Program.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+using Reflection;
+using System.Diagnostics;
+class Program
+
+{
+    static void Main(string[] args)
+    {
+        F fInstance = F.Get();
+
+        // 3. Замер времени сериализации
+        int iterations = 1000;
+        string serializedString = null;
+
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        for (int i = 0; i < iterations; i++)
+        {
+            serializedString = fInstance.Serialize();
+        }
+
+        stopwatch.Stop();
+        Console.WriteLine($"Сериализация: {serializedString}");
+        Console.WriteLine($"Время на сериализацию (пользовательская): {stopwatch.ElapsedMilliseconds} мс");
+
+        // 7. Сериализация с использованием Newtonsoft.Json
+        stopwatch.Reset();
+        stopwatch.Start();
+
+        string jsonString = null;
+
+        for (int i = 0; i < iterations; i++)
+        {
+            jsonString = JsonConvert.SerializeObject(fInstance);
+        }
+
+        stopwatch.Stop();
+        Console.WriteLine($"JSON Сериализация: {jsonString}");
+        Console.WriteLine($"Время на сериализацию (Newtonsoft.Json): {stopwatch.ElapsedMilliseconds} мс");
+
+        // 9. Десериализация из строки
+        stopwatch.Reset();
+        stopwatch.Start();
+
+        F deserializedInstance = null;
+
+        for (int i = 0; i < iterations; i++)
+        {
+            deserializedInstance = F.Deserialize(serializedString);
+        }
+
+        stopwatch.Stop();
+        Console.WriteLine($"Десериализация: i1={deserializedInstance.i1}, i2={deserializedInstance.i2}, i3={deserializedInstance.i3}, i4={deserializedInstance.i4}, i5={deserializedInstance.i5}");
+        Console.WriteLine($"Время на десериализацию (пользовательская): {stopwatch.ElapsedMilliseconds} мс");
+
+        // 11. Десериализация с использованием Newtonsoft.Json
+        stopwatch.Reset();
+        stopwatch.Start();
+
+        F jsonDeserializedInstance = null;
+
+        for (int i = 0; i < iterations; i++)
+        {
+            jsonDeserializedInstance = JsonConvert.DeserializeObject<F>(jsonString);
+        }
+
+        stopwatch.Stop();
+        Console.WriteLine($"JSON Десериализация: i1={jsonDeserializedInstance.i1}, i2={jsonDeserializedInstance.i2}, i3={jsonDeserializedInstance.i3}, i4={jsonDeserializedInstance.i4}, i5={jsonDeserializedInstance.i5}");
+        Console.WriteLine($"Время на десериализацию (Newtonsoft.Json): {stopwatch.ElapsedMilliseconds} мс");
+    }
+}
